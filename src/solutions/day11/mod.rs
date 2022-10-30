caff9faac9560deb5183b0d3c71a25204b6f23e5
zyleo <liuzy199901@gmail.com>
2022-10-30 13:33:57 +0800

    finished day11

--- src/solutions/day11/mod.rs
+++ src/solutions/day11/mod.rs
@@ -33,7 +33,7 @@ pub fn solve_day11_part1() -> usize {
 #[allow(dead_code)]
 fn flash(g: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
     // dfs
-    g[x][y] = b'0';
+    g[x][y] = b'0'; // 0 means it just flashed
     DIRS.iter()
         .map(|(xx, yy)| ((x as isize + xx) as usize, (y as isize + yy) as usize))
         .fold(1, |acc, (x, y)| {
@@ -45,4 +45,58 @@ fn flash(g: &mut Vec<Vec<u8>>, x: usize, y: usize) -> usize {
                 _ => acc,
             }
         })
+}
+
+#[allow(dead_code)]
+pub fn solve_day11_part2() -> usize {
+    let mut g = include_bytes!("../../../data/day11/input")
+        .split(|&b| b == b'\n')
+        .map(|l| l.to_vec())
+        .collect::<Vec<Vec<_>>>();
+
+    let mut ans = 0;
+    // (0..300).for_each(|_| {
+    loop {
+        ans += 1;
+        g.iter_mut()
+            .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
+
+        (0..SIZE)
+            .flat_map(|x| (0..SIZE).map(move |y| (x, y)))
+            .for_each(|(x, y)| {
+                if g[x][y] > b'9' {
+                    flash(&mut g, x, y);
+                }
+            });
+
+        // println!("round {ans}");
+        // g.iter().for_each(|row| {
+        //     row.iter().for_each(|cell| print!("{} ", *cell as char));
+        //     println!("");
+        // });
+
+        if g.iter().all(|row| row.iter().all(|cell| *cell == b'0')) {
+            break;
+        }
+    }
+    ans
+
+    // ================
+    // example solution
+    // ================
+    // https://github.com/timvisee/advent-of-code-2021/blob/master/day11b/src/main.rs
+    // (1..)
+    //     .find(|_| {
+    //         g.iter_mut()
+    //             .for_each(|row| row.iter_mut().for_each(|cell| *cell += 1));
+
+    //         // count the flash times
+    //         (0..SIZE)
+    //             .flat_map(|x| (0..SIZE).map(move |y| (x, y)))
+    //             .fold(0, |acc, (x, y)| {
+    //                 acc + (g[x][y] > b'9').then(|| flash(&mut g, x, y)).unwrap_or(0)
+    //             })
+    //             == SIZE * SIZE
+    //     })
+    //     .unwrap()
 }
